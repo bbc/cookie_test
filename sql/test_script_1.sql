9ebf504c65c7ff3dccb92fce9574318172772bf1
Vicky Banks <victoria.banks052@gmail.com>
2021-04-29 11:24:07 +0100

    Corrected how to call the parameter

--- sql/test_script_1.sql
+++ sql/test_script_1.sql
@@ -12,20 +12,18 @@ CREATE TABLE IF NOT EXISTS dataforce_sandbox.vb_cookie_test
  GRANT ALL ON dataforce_sandbox.vb_cookie_test TO dataforce_analytics_priority ;
   */
 
-/*
+
 INSERT INTO dataforce_sandbox.vb_cookie_test
 SELECT dt, visit_id
 FROM s3_audience.publisher
-WHERE destination = 'PS_IPLAYER' AND dt = run_date::varchar LIMIT 1;
+WHERE destination = 'PS_IPLAYER' AND dt = <params.run_date> LIMIT 1;
 
 INSERT INTO dataforce_sandbox.vb_cookie_test
 SELECT dt,visit_id
 FROM s3_audience.publisher
-WHERE destination = 'PS_IPLAYER' AND dt = previous_run_date::varchar  LIMIT 1;
-*/
+WHERE destination = 'PS_IPLAYER' AND dt = <params.previous_run_date>  LIMIT 1;
 
-DROP TABLE IF EXISTS dataforce_sandbox.vb_cookie_test;
-CREATE TABLE IF NOT EXISTS dataforce_sandbox.vb_cookie_test AS
-    SELECT run_date as dt, previous_run_date as prev_dt;
+
+
 
 GRANT ALL ON dataforce_sandbox.vb_cookie_test TO vicky_banks ;
